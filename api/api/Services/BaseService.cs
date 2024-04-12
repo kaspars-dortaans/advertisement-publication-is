3758edfc667036a29189d778de6b0e1e961ae431
Kaspars_Dortans <kaspars.dortans@gmail.com>
2024-04-12 19:49:12 +0300

    created base service

---
+++ api/api/Services/BaseService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace api.Services
+{
+    public class BaseService<Entity> : IBaseService<Entity> where Entity : class
+    {
+        protected DbSet<Entity> DbSet { get; set; }
+
+        public BaseService(DbSet<Entity> set) {
+            DbSet = set;
+        }
+
+        public IEnumerable<Entity> GetAll()
+        {
+            return DbSet.ToList();
+        }
+    }
+}
---
+++ api/api/Services/IBaseService.cs
@@ -0,0 +1,7 @@
+﻿namespace api.Services
+{
+    public interface IBaseService<Entity> where Entity : class
+    {
+        public IEnumerable<Entity> GetAll();
+    }
+}
