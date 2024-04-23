7bb76d9224ff6c6be7125e8ccd7b9a46a23483b1
Kaspars Dortāns <59746217+kaspars-dortaans@users.noreply.github.com>
2024-04-24 01:48:43 +0300

    implemented DataTableQueryResolver

--- api/api/Helpers/DataTableQueryResolver.cs
+++ api/api/Helpers/DataTableQueryResolver.cs
@@ -3,7 +3,6 @@ using api.Dto.DataTableQuery;
 using api.Dto.ListQuery;
 using AutoMapper;
 using System.Linq.Expressions;
-using System.Reflection;
 
 namespace api.Helpers;
 
@@ -21,7 +20,7 @@ public static class DataTableQueryResolver
         var searchableColumns = request.Columns.Where(c => c.Searchable).ToList();
         if (!string.IsNullOrEmpty(request.Search?.Value) && searchableColumns.Any())
         {
-            query = query.Where(GetWhereSearchPredicate<Entity>(searchableColumns.Select(c => c.Name).ToList(), request.Search.Value));
+            query = query.Where(ReflectionHelper.GetWhereSearchPredicate<Entity>(searchableColumns.Select(c => c.Name).ToList(), request.Search.Value));
         }
 
         //Filter
@@ -30,7 +29,7 @@ public static class DataTableQueryResolver
         {
             foreach (var column in filteredColumns)
             {
-                query = query.Where(GetWhereSearchPredicate<Entity>(new List<string>() { column.Name }, column.Search!.Value));
+                query = query.Where(ReflectionHelper.GetWhereSearchPredicate<Entity>(new List<string>() { column.Name }, column.Search!.Value));
             }
         }
 
@@ -65,6 +64,14 @@ public static class DataTableQueryResolver
         };
     }
 
+    /// <summary>
+    /// Maps DataTableQueryResponse data to specified type
+    /// </summary>
+    /// <typeparam name="Entity"></typeparam>
+    /// <typeparam name="Dto"></typeparam>
+    /// <param name="mapper"></param>
+    /// <param name="response"></param>
+    /// <returns>DataTableResponse with mapped response data</returns>
     public static DataTableQueryResponse<Dto> MapDataTableResult<Entity, Dto>(this IMapper mapper, DataTableQueryResponse<Entity> response)
     {
         return new DataTableQueryResponse<Dto>()
@@ -77,137 +84,60 @@ public static class DataTableQueryResolver
         };
     }
 
-    //TODO: Improve field search?
+    /// <summary>
+    /// Applies sorting to IQueryable described with DataTableQuery
+    /// </summary>
+    /// <typeparam name="Entity"></typeparam>
+    /// <param name="query"></param>
+    /// <param name="request"></param>
+    /// <returns>IQueryable with applied sorting</returns>
     static IQueryable<Entity> OrderQuery<Entity>(IQueryable<Entity> query, DataTableQuery request)
     {
-        var order = request.Order.ToList();
-        var columnName = request.Columns.ElementAt(order[0].Column).Name;
-        var columnType = typeof(Entity).GetProperty(columnName)!.PropertyType;
-        var keySelectorType = typeof(Func<,>).MakeGenericType(new Type[] { typeof(Entity), columnType });
-        var keySelectorExpressionType = typeof(Expression<>).MakeGenericType(keySelectorType);
-
-        var methodName = order[0].Direction == Direction.Ascending ? nameof(Queryable.OrderBy) : nameof(Queryable.OrderByDescending);
-
-        var keySelectorLambda = InvokeGenericMethode<Expression>(
-            typeof(DataTableQueryResolver),
-            nameof(GetKeySelectorLambda),
-            new Type[] { typeof(string) },
-            new Type[] { typeof(Entity), columnType },
-            new object[] { columnName });
-
-
-        query = InvokeGenericMethode<IOrderedQueryable<Entity>>(
-            typeof(Queryable),
-            methodName,
-            new Type[] { typeof(IQueryable<Entity>), keySelectorExpressionType },
-            new Type[] { typeof(Entity), columnType },
-            new object[] { query, keySelectorLambda });
-
-        if (order.Count > 1)
+        var orderList = request.Order.ToList();
+        var secondarySort = false;
+        string ascendingOrderMethodName = nameof(Queryable.OrderBy), descendingOrderMethodName = nameof(Queryable.OrderByDescending);
+        for(var i = 0; i < orderList.Count; i++)
         {
-            for (var i = 0; i < order.Count; i++)
+            var sortApplied = ApplySort(ascendingOrderMethodName, descendingOrderMethodName, orderList[i]);
+            if(!secondarySort && sortApplied)
             {
-                columnName = request.Columns.ElementAt(order[i].Column).Name;
-                columnType = typeof(Entity).GetProperty(columnName)!.GetType();
-                methodName = order[0].Direction == Direction.Ascending ? nameof(Queryable.ThenBy) : nameof(Queryable.ThenByDescending);
-                keySelectorLambda = InvokeGenericMethode<Expression>(
-                    typeof(DataTableQueryResolver),
-                    nameof(GetKeySelectorLambda),
-                    new Type[] { typeof(string) },
-                    new Type[] { typeof(Entity), columnType },
-                    new object[] { columnName });
-
-                query = InvokeGenericMethode<IOrderedQueryable<Entity>>(
-                    typeof(Queryable),
-                    methodName,
-                    new Type[] { typeof(IQueryable<Entity>), keySelectorExpressionType },
-                    new Type[] { typeof(Entity), columnType },
-                    new object[] { query, keySelectorLambda });
+                ascendingOrderMethodName = nameof(Queryable.ThenBy);
+                descendingOrderMethodName = nameof(Queryable.ThenByDescending);
             }
         }
 
         return query;
-    }
 
-    static TResult InvokeGenericMethode<TResult>(Type objType, string methodName, Type[] methodArgumentTypes, Type[] typeParameters, object[] methodParameters, object? currentInstance = null) where TResult : class
-    {
-        var method = objType
-            .GetMethods()
-            .Where(m => m.Name == methodName && m.GetGenericArguments().Length == typeParameters.Length && m.GetParameters().Length == methodArgumentTypes.Length)
-            .First();
-
-        var generic = method!.MakeGenericMethod(typeParameters);
-        var result = generic.Invoke(currentInstance, methodParameters);
-        return (result as TResult)!;
-    }
-
-    static Expression GetPropertyExpression(Expression pe, string chain)
-    {
-        var properties = chain.Split('.');
-        foreach (var property in properties)
-            pe = Expression.Property(pe, property);
-
-        return pe;
-    }
-
-    /// <summary>
-    /// Get lambda predicate expression. Which searches for string in objects fields. Object mathes if any of the fields values contains search value, both strings are converted to lowercase. 
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="searchFieldList"></param>
-    /// <param name="searchValue"></param>
-    /// <returns>Expression containing lambda function predicate</returns>
-    public static Expression<Func<T, bool>> GetWhereSearchPredicate<T>(IEnumerable<string> searchFieldList, string searchValue)
-    {
-        //the 'IN' parameter for expression ie T=> condition
-        ParameterExpression pe = Expression.Parameter(typeof(T), typeof(T).Name);
-
-        //combine them with and 1=1 Like no expression
-        Expression? combined = null;
-
-        if (searchFieldList != null)
+        //Returns true if sort was applied
+        bool ApplySort(string orderAscendingMethodName, string orderDescendingMethodName, OrderQuery order)
         {
-            var toLowerMethod = typeof(string).GetMethod("ToLower", BindingFlags.Public | BindingFlags.Instance, Array.Empty<Type>());
-            var containsMethod = typeof(string).GetMethod("Contains", BindingFlags.Public | BindingFlags.Instance, new Type[] { typeof(string) });
-            var searchValueLowercase = Expression.Call(Expression.Constant(searchValue), toLowerMethod);
-
-            foreach (var fieldItem in searchFieldList)
+            var columnName = request.Columns.ElementAt(order.Column).Name;
+            var columnType = typeof(Entity).GetProperty(columnName)?.PropertyType;
+            if (columnType == null)
             {
-                //Expression for accessing Fields name property
-                var entityProperty = GetPropertyExpression(pe, fieldItem);
-
-                if(entityProperty.Type != typeof(string))
-                {
-                    var toStringMethod = entityProperty.Type.GetMethod("ToString", BindingFlags.Public | BindingFlags.Instance, new Type[] { });
-                    entityProperty = Expression.Call(entityProperty, toStringMethod);
-                }
-
-                var propertyLowercase = Expression.Call(entityProperty, toLowerMethod);
-                var containsExporession = Expression.Call(propertyLowercase, containsMethod, new Expression[] { searchValueLowercase });
-
-                if (combined == null)
-                {
-                    combined = containsExporession;
-                }
-                else
-                {
-                    combined = Expression.Or(combined, containsExporession);
-                }
+                return false;
             }
+
+            var keySelectorType = typeof(Func<,>).MakeGenericType(new Type[] { typeof(Entity), columnType });
+            var keySelectorExpressionType = typeof(Expression<>).MakeGenericType(keySelectorType);
+            var methodName = order.Direction == Direction.Ascending ? orderAscendingMethodName : orderDescendingMethodName;
+
+            var keySelectorLambda = ReflectionHelper.InvokeGenericMethode<Expression>(
+                typeof(ReflectionHelper),
+                nameof(ReflectionHelper.GetKeySelectorLambda),
+                new Type[] { typeof(string) },
+                new Type[] { typeof(Entity), columnType },
+                new object[] { columnName });
+
+
+            query = ReflectionHelper.InvokeGenericMethode<IOrderedQueryable<Entity>>(
+                typeof(Queryable),
+                methodName,
+                new Type[] { typeof(IQueryable<Entity>), keySelectorExpressionType },
+                new Type[] { typeof(Entity), columnType },
+                new object[] { query, keySelectorLambda });
+            
+            return true;
         }
-
-        //create and return the predicate
-        return Expression.Lambda<Func<T, bool>>(combined ?? Expression.Constant(true), new ParameterExpression[] { pe });
     }
-
-    public static Expression<Func<T, TKey>> GetKeySelectorLambda<T, TKey>(string fieldName)
-    {
-        //the 'IN' parameter for expression ie T=> condition
-        ParameterExpression pe = Expression.Parameter(typeof(T), typeof(T).Name);
-
-        var propertyExpression = GetPropertyExpression(pe, fieldName);
-
-        return Expression.Lambda<Func<T, TKey>>(propertyExpression, new ParameterExpression[] { pe });
-    }
-
 }
---
+++ api/api/Helpers/ReflectionHelper.cs
@@ -0,0 +1,113 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+
+namespace api.Helpers;
+
+public static class ReflectionHelper
+{
+    /// <summary>
+    /// Invokes Generic method from passed type, method is searched by name, argument count and type parameter count
+    /// </summary>
+    /// <typeparam name="TResult"></typeparam>
+    /// <param name="objType"></param>
+    /// <param name="methodName"></param>
+    /// <param name="methodArgumentTypes"></param>
+    /// <param name="typeParameters"></param>
+    /// <param name="methodParameters"></param>
+    /// <param name="currentInstance"></param>
+    /// <returns>Invoked method result</returns>
+    public static TResult InvokeGenericMethode<TResult>(Type objType, string methodName, Type[] methodArgumentTypes, Type[] typeParameters, object[] methodParameters, object? currentInstance = null) where TResult : class
+    {
+        var method = objType
+            .GetMethods()
+            .Where(m => m.Name == methodName && m.GetGenericArguments().Length == typeParameters.Length && m.GetParameters().Length == methodArgumentTypes.Length)
+            .First();
+
+        var generic = method!.MakeGenericMethod(typeParameters);
+        var result = generic.Invoke(currentInstance, methodParameters);
+        return (result as TResult)!;
+    }
+
+    /// <summary>
+    /// Builds property expression ()
+    /// </summary>
+    /// <param name="parameterExpression"></param>
+    /// <param name="chain"></param>
+    /// <returns>Property expression</returns>
+    public static Expression GetPropertyExpression(Expression parameterExpression, string chain)
+    {
+        var properties = chain.Split('.');
+        foreach (var property in properties)
+            parameterExpression = Expression.Property(parameterExpression, property);
+
+        return parameterExpression;
+    }
+
+    /// <summary>
+    /// Get lambda predicate expression. Which searches for string in objects fields. Object mathes if any of the fields values contains search value, both strings are converted to lowercase. 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="searchFieldList"></param>
+    /// <param name="searchValue"></param>
+    /// <returns>Expression containing lambda function predicate</returns>
+    public static Expression<Func<T, bool>> GetWhereSearchPredicate<T>(IEnumerable<string> searchFieldList, string searchValue)
+    {
+        //the 'IN' parameter for expression ie T=> condition
+        ParameterExpression pe = Expression.Parameter(typeof(T), typeof(T).Name);
+
+        //combine them with and 1=1 Like no expression
+        Expression? combined = null;
+
+        if (searchFieldList != null)
+        {
+            var toLowerMethod = typeof(string).GetMethod("ToLower", BindingFlags.Public | BindingFlags.Instance, Array.Empty<Type>())!;
+            var containsMethod = typeof(string).GetMethod("Contains", BindingFlags.Public | BindingFlags.Instance, new Type[] { typeof(string) })!;
+            var searchValueLowercase = Expression.Call(Expression.Constant(searchValue), toLowerMethod);
+
+            foreach (var fieldItem in searchFieldList)
+            {
+                //Expression for accessing Fields name property
+                var entityProperty = GetPropertyExpression(pe, fieldItem);
+
+                if (entityProperty.Type != typeof(string))
+                {
+                    var toStringMethod = entityProperty.Type.GetMethod("ToString", BindingFlags.Public | BindingFlags.Instance, new Type[] { });
+                    entityProperty = toStringMethod != null ? Expression.Call(entityProperty, toStringMethod) : Expression.Constant(string.Empty);
+                }
+
+                var propertyLowercase = Expression.Call(entityProperty, toLowerMethod);
+                var containsExporession = Expression.Call(propertyLowercase, containsMethod, new Expression[] { searchValueLowercase });
+
+                if (combined == null)
+                {
+                    combined = containsExporession;
+                }
+                else
+                {
+                    //Be sure to use Expression.OrElse to not run in to bug: https://github.com/dotnet/efcore/issues/30181
+                    combined = Expression.OrElse(combined, containsExporession);
+                }
+            }
+        }
+
+        //create and return the predicate
+        return Expression.Lambda<Func<T, bool>>(combined ?? Expression.Constant(true), new ParameterExpression[] { pe });
+    }
+
+    /// <summary>
+    /// Builds expression with lamba function which returns property
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="TKey"></typeparam>
+    /// <param name="fieldName"></param>
+    /// <returns>Expression with lambda function which returns property</returns>
+    public static Expression<Func<T, TKey>> GetKeySelectorLambda<T, TKey>(string fieldName)
+    {
+        //the 'IN' parameter for expression ie T=> condition
+        ParameterExpression pe = Expression.Parameter(typeof(T), typeof(T).Name);
+
+        var propertyExpression = GetPropertyExpression(pe, fieldName);
+
+        return Expression.Lambda<Func<T, TKey>>(propertyExpression, new ParameterExpression[] { pe });
+    }
+}
