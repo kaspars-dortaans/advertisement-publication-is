f0fc7007a0a73845303aaa494b7f3a6a0e55cc02
Kaspars_Dortans <kaspars.dortans@gmail.com>
2025-01-05 14:45:47 +0200

    LocaleService refactored locale loading

--- client-app/src/services/locale-service.ts
+++ client-app/src/services/locale-service.ts
@@ -1,11 +1,35 @@
-import { axiosInstance } from '@/init/axios'
-import { usePrimeVue } from 'primevue/config'
+import { usePrimeVue, type PrimeVueLocaleOptions } from 'primevue/config'
+import { ref } from 'vue'
 
 export class LocaleService {
-  _primevue: ReturnType<typeof usePrimeVue> | undefined
+  private static _instance: LocaleService
+  private _primevue: ReturnType<typeof usePrimeVue> | undefined
+  private _localeFiles: { [key: string]: () => Promise<unknown> } = import.meta.glob(
+    '@/locales/*.json'
+  )
+  private _localeMap: Map<string, string>
+  private _localeList: string[]
+  private _currentLocale = ref('')
 
-  constructor(primevue?: ReturnType<typeof usePrimeVue>) {
-    this._primevue = primevue
+  public static get(primevue?: ReturnType<typeof usePrimeVue>) {
+    if (!this._instance) {
+      this._instance = new LocaleService()
+    }
+    this._instance._primevue = primevue
+    return this._instance
+  }
+
+  private constructor() {
+    this._localeMap = new Map()
+    this._localeList = []
+
+    for (const filePath in this._localeFiles) {
+      const filePathSplit = filePath.split('/')
+      const fileName = filePathSplit[filePathSplit.length - 1]
+      const localeName = fileName.substring(0, fileName.lastIndexOf('.')).toLocaleUpperCase()
+      this._localeMap.set(localeName, filePath)
+      this._localeList.push(localeName)
+    }
   }
 
   get primevue() {
@@ -14,14 +38,28 @@ export class LocaleService {
     return this._primevue
   }
 
-  async loadLocale(name: string) {
-    const locale = await this.fetchLocale(name)
-    this.primevue.config.locale = locale.data
+  /**
+   * Return available locale list. Do NOT modify it
+   */
+  get localeList() {
+    return this._localeList
   }
 
-  async fetchLocale(name: string) {
-    const localePath = `src/locales/${name}.json`
-    return await axiosInstance.get(localePath, { baseURL: import.meta.env.BASE_URL })
+  get currentLocale() {
+    return this._currentLocale
+  }
+
+  async loadLocale(name: string) {
+    const nameNormalized = name.toLocaleUpperCase()
+    if (!this._localeList.some((value) => value === nameNormalized)) {
+      //Locale not found, return
+      return
+    }
+
+    this._currentLocale.value = nameNormalized
+    const filePath = this._localeMap.get(nameNormalized)!
+    const locale = await this._localeFiles[filePath]()
+    this.primevue.config.locale = locale as PrimeVueLocaleOptions
   }
 
   localizeMultiple(keys: string[], suffix?: string, ...params: string[]) {
@@ -53,7 +91,6 @@ export class LocaleService {
       prefixIndex = -1,
       suffixIndex = -1
     for (const param of params) {
-      console.log('param ', param)
       prefixIndex = result.indexOf(paramsPrefix, lastParamIndex)
       suffixIndex = result.indexOf(paramSuffix, prefixIndex)
       if (prefixIndex >= 0 && suffixIndex >= 0) {
