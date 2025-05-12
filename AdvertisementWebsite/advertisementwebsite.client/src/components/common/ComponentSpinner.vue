74ccad8c7f9bc118d40df1c7c8d359e1c37b7264
Kaspars_Dortans <kaspars.dortans@gmail.com>
2025-05-12 10:01:15 +0300

    menu dropdown style fix

--- AdvertisementWebsite/advertisementwebsite.client/src/components/common/ComponentSpinner.vue
+++ AdvertisementWebsite/advertisementwebsite.client/src/components/common/ComponentSpinner.vue
@@ -1,12 +1,18 @@
 <template>
   <div
     v-if="show"
-    class="absolute top-0 left-0 w-full h-full z-[3000] flex justify-center items-center"
+    class="absolute top-0 left-0 w-full h-full flex justify-center items-center"
+    :style="{ 'z-index': zIndex }"
   >
     <i class="pi pi-spin pi-spinner text-white text-[2rem]"></i>
   </div>
 </template>
 
 <script setup lang="ts">
+import { usePrimeVue } from 'primevue'
+import { computed } from 'vue'
+
 defineProps<{ show: boolean }>()
+const primevue = usePrimeVue()
+const zIndex = computed(() => (primevue?.config?.zIndex?.modal ?? 1000) + 10)
 </script>
